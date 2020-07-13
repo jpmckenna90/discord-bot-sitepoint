116815e78552d88ed2e4f7f19b1213c9d2256da7
jpm <jpmckenna90@gmail.com>
2020-07-13 15:56:28 -0400

    Updated functionality to respond to tags, added express server to host on heroku

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
+const express = require("express");
+const app = express();
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const PORT = 3030 || process.env.PORT;
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
 
 bot.login(TOKEN);
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+bot.on("message", (msg) => {
+  if (msg.mentions.users.first().username === "lieDetector") {
+    msg.reply(messages[Math.floor(Math.random() * messages.length)]);
   }
 });
+
+const messages = [
+  "lol yea",
+  "Are you kidding? Of course he is.",
+  "idk he might not be...",
+  "look at that nose grow damn",
+  "call my dude Jim Carrey cause he's a liar liar",
+  "does Renly shit in the grass?",
+  "If lying is cool, consider him Miles Davis",
+  "liar.com/bigfatliar/omgwhataliar",
+];
